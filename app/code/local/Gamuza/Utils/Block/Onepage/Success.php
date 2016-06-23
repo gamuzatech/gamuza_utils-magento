973f049456202fdfa9567c8e2f483c29bf1a3f18
Eneias Ramos de Melo <eneias@gamuza.com.br>
2016-06-23 01:07:31 -0300

    Ajuste para exibir descrição do pedido na pagina de sucesso para clientes convidados

---
+++ app/code/local/Gamuza/Utils/Block/Onepage/Success.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Gamuza Utils - Basic features for magento platform.
+ * Copyright (C) 2016 Gamuza Technologies (http://www.gamuza.com.br/)
+ * Author: Eneias Ramos de Melo <eneias@gamuza.com.br>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+/*
+ * See the AUTHORS file for a list of people on the Gamuza Team.
+ * See the ChangeLog files for a list of changes.
+ * These files are distributed with gamuza_utils-magento at http://github.com/gamuzabrasil/.
+ */
+
+class Gamuza_Utils_Block_Onepage_Success
+extends Mage_Checkout_Block_Onepage_Success
+// extends Mage_Core_Block_Template
+{
+
+public function getCanViewOrder ()
+{
+    return true; // to view orders as a guest.
+}
+
+}
+
